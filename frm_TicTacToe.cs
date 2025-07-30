7f327f39690d47784ec4974f9c0c155678849695
CTH-npitx <chatch@npitx.org>
2025-07-30 15:19:10 -0500

    Revert "Merge branch 'debug-form-2' into debug-form"
    This reverts commit 024c03e0cd0f47842fb1c79182279dfc24d63e6b, reversing
    changes made to 0b134373a7de9ece11a9c2743cd86defb2dc1470.

--- frm_TicTacToe.cs
+++ frm_TicTacToe.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,5 +17,16 @@ namespace Tic_Tac_Toe_TTC
         {
             InitializeComponent();
         }
+        private void frm_main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var frm = Application.OpenForms[frm_info];
+        }
+
+
     }
 }
