9ae6b6b86b871f0c28876bb090b8c05ebbecf33e
CTH-npitx <chatch@npitx.org>
2025-08-07 12:32:52 -0500

    Undo all changes
    undo all the changes, to allow redoing them with improvments

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TicTacToeProject")]
+[assembly: AssemblyTitle("Tic Tac Toe TTC")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("nonPareil Institute")]
-[assembly: AssemblyProduct("TicTacToeProject")]
+[assembly: AssemblyProduct("Tic Tac Toe TTC")]
 [assembly: AssemblyCopyright("Copyright © nonPareil Institute 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6a871a9c-5d6e-41c8-b838-ba3468956805")]
+[assembly: Guid("58fea966-10d7-4925-aee5-6478203f8f92")]
 
 // Version information for an assembly consists of the following four values:
 //
