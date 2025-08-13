110dd5cfe9b1209377abb8b67dd150bf2a5690a4
Um... Figure it out! <33612115+CTH999@users.noreply.github.com>
2025-08-13 15:12:12 -0500

    Update coinFlip.cs

--- coinFlip.cs
+++ coinFlip.cs
@@ -30,6 +30,6 @@ namespace TicTacToe
 
             randVal = randVal % 2;
             return randVal;
-        } //coin flip algorithim. To run, use: 
+        } //coin flip algorithim. To run, use: TicTacToe.randomize.coinFlip()
     }
 }
