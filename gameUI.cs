7bd2b5d35fd51ba46dbe4f536d548ebb87804888
CTH-npitx <chatch@npitx.org>
2025-09-03 14:29:30 -0500

    uodate name

--- gameUI.cs
+++ gameUI.cs
@@ -10,13 +10,13 @@ using System.Windows.Forms;
 
 namespace TicTacToe
 {
-    public partial class Form1 : Form
+    public partial class gameUI : Form
     {
         static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };   //initializes array of characters representing Tic Tac Toe board, each index corresponds to a position on the board with '0' to '9' serving as placeholders for player moves
         static int player = 1;  //keeps track of current player starting with player 1 & toggling between player 1 and player 2 as game progresses
         static int choice;  //stores player's selected position on board & is updated based on user input during game
         static int flag = 0;    //determines state of game & can indicate whether game is ongoing, a player has won, or if it has ended in draw
-        public Form1()
+        public gameUI()
         {
             InitializeComponent();
         }
