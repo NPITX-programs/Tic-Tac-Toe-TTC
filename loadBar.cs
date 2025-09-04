7bf324ab4070cfeee6ab5da2b178a98f7d31904e
CTH-npitx <chatch@npitx.org>
2025-09-04 11:27:52 -0500

    progres bar
    take progress bar object and the desired max value (defaults to 100)
    
    a version of this method might be made which will extract the maximum from the input progress bar object. This current one allows you to manipulate it internally

--- loadBar.cs
+++ loadBar.cs
@@ -4,14 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
+using System.Windows.Forms;
 
 namespace TicTacToeProject
 {
     internal class loadBar
     {
-        internal static void LoadBar(int loadTime = 100, int total = 20)
+        internal static void LoadBar(ProgressBar progressBar, int max = 100)
         {
-
+            progressBar.Maximum = max;
         } 
     }
 }
