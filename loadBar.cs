829e600b6f16d07357469305817f4f8e5f9eb944
CTH-npitx <chatch@npitx.org>
2025-09-04 11:20:01 -0500

    Update loadBar.cs

--- loadBar.cs
+++ loadBar.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace TicTacToeProject
 {
     internal class loadBar
     {
+        internal static void LoadBar(int loadTime = 100, int total = 20)
+        {
+
+        } 
     }
 }
