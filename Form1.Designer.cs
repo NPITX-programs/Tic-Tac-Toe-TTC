4aef4ef4ef4014344cf09d98e3dcb2aba5377bc1
rwoods <rwoods@HOU-WK-5045>
2025-09-10 15:57:44 -0500

    settings menu

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,6 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
             this.ClientSize = new System.Drawing.Size(351, 450);
             this.Name = "Settings";
             this.Text = "Settings";
